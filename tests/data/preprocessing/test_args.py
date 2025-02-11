29bf59b9abdf6d548daf4990dc4c79cf9ed5cc04
salvatore <salvatorelaiso@gmail.com>
2025-02-11 13:52:17 +0100

    test: improve `Args` tests for `pylon_experiments.data.preprocessing.args`

--- tests/data/preprocessing/test_args.py
+++ tests/data/preprocessing/test_args.py
@@ -1,4 +1,5 @@
 import pathlib
+from contextlib import nullcontext as does_not_raise
 
 import pytest
 
@@ -6,18 +7,26 @@ from pylon_experiments.data.preprocessing.args import Args
 
 
 @pytest.mark.parametrize(
-    "path, expected_path",
+    "path, expected_path, expectation",
     [
         (
             "data/mydata",
             pathlib.Path("data/mydata"),
+            does_not_raise(),
         ),
         (
             pathlib.Path("data/mydata"),
             pathlib.Path("data/mydata"),
+            does_not_raise(),
+        ),
+        (
+            None,
+            None,
+            pytest.raises(Exception),
         ),
     ],
 )
-def test_args_path_field(path, expected_path):
-    args = Args(path=path)
-    assert args.path == expected_path
+def test_args_path_field(path, expected_path, expectation):
+    with expectation:
+        args = Args(path=path)
+        assert args.path == expected_path
